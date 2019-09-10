9ffe525e48e883992f35af0cd30975116c6d3f16
Calvin Tiley <calvintiley@hotmail.co.uk>
2019-09-11 00:05:04 +0100

    Added placeholder file for integration tests

---
+++ tests/integration/Example.unit.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Example from '../../src/components/Example/Example';
+
+test('<Example />', () => {
+    expect(true).toBeTruthy();
+});
