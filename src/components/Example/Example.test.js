bcca282668d4f603fbf0a4acb978eb7b9e29a2db
Calvin Tiley <calvintiley@hotmail.co.uk>
2019-11-09 22:43:52 +0000

    Added redux-mock-store to simulate stores for components

--- src/components/Example/Example.test.js
+++ src/components/Example/Example.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 
-import Example from './index';
+import Example from './Example';
+
+const mockStore = configureMockStore()({});
 
 describe('Example Container Test', () => {
    it('renders without crashing', () => {
-      shallow(<Example />);
+      expect(
+         shallow(
+            <Provider store={mockStore}>
+               <Example />;
+            </Provider>
+         )
+      );
     });
 });
