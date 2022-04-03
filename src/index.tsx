8febc6a5de576bfe4780086be581123bcff28f6c
Calvin Tiley <calvintiley@hotmail.co.uk>
2022-04-03 22:17:45 +0100

    feat: hooked up redux, managed dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,16 @@
 import ReactDOM from 'react-dom' ;
 import { Provider } from 'react-redux';
-import { getContext, resetContext } from 'kea';
-import App from './App/App';
 
+import App from './App/App';
+import store from './store';
 import './scss/entry.scss';
 
 // Imports SVGs so webpack loaders generate spritesheet
 const svgs = require.context('./assets/icons/', true, /\.svg$/);
 svgs.keys().forEach(svgs);
 
-resetContext({
-    createStore: {},
-    plugins: [],
-})
-
 ReactDOM.render(
-    <Provider store={getContext().store}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById('root')
---
+++ src/store/index.tsx
@@ -0,0 +1,8 @@
+import { applyMiddleware, createStore, Store } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers';
+
+const store: Store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
