2ebc7f24710683fd6441b476f4d7cfbc41d8c87b
Calvin Tiley <calvintiley@hotmail.co.uk>
2023-01-14 18:49:59 +0000

    feat: favicon loading

--- webpack.development.js
+++ webpack.development.js
@@ -1,21 +1,22 @@
-const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'development',
+    mode: "development",
 
     devServer: {
         port: 3000,
         historyApiFallback: true,
     },
 
-    devtool: 'eval-source-map',
+    devtool: "eval-source-map",
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'src/index.html',
+            template: "src/index.html",
+            favicon: "./src/assets/icons/favicon.ico",
         }),
     ],
 
@@ -25,10 +26,10 @@ module.exports = merge(common, {
                 test: /\.s[ac]ss$/,
                 exclude: /node_modules/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
                 ],
             },
         ],
--- webpack.production.js
+++ webpack.production.js
@@ -1,16 +1,17 @@
-const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'production',
+    mode: "production",
 
     optimization: {
         minimizer: [
             new HtmlWebpackPlugin({
-                template: 'src/index.html',
+                favicon: "./src/assets/icons/favicon.ico",
+                template: "src/index.html",
             }),
             new CssMinimizerPlugin(),
         ]
@@ -22,10 +23,10 @@ module.exports = merge(common, {
                 test: /\.(sass|s?css)$/,
                 exclude: /node_modules/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
                 ],
             },
         ],
